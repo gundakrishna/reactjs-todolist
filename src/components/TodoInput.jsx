e46a95c4cae5826fcc00c2bfdf80e6182126eaba
Krishna Gunda <gundakrishna.jobs@gmail.com>
2024-06-24 21:51:20 +0530

    Built React Todo List App

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function TodoInput(props) {
+  const {handleAddTodos, todoValue, setTodoValue} = props
+  
+  return (
+    <header>
+      <input placeholder='Enter todo...' value={todoValue} onChange={(e) => {setTodoValue(e.target.value)}} />
+      <button onClick={() => { 
+        handleAddTodos(todoValue) 
+        setTodoValue('')
+      }}>Add</button>
+    </header>
+  )
+}
+
+export default TodoInput
